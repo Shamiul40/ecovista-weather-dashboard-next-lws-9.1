45ea1763d9bee49231c759e91f0ce0f788400c0c
Shamiul40 <shamiul4040@gmail.com>
2026-02-19 01:16:48 +0600

    data update

--- app/api/location/route.js
+++ app/api/location/route.js
@@ -1,12 +1,9 @@
 import { NextResponse } from "next/server";
-import { getLocation } from "./location-utils"
+import { getLocation } from "./location-utils";
 
+export const GET = async (request) => {
+  const location = await getLocation();
+  console.log(location);
 
-export const GET=async(request)=>{
-
-    const location = await getLocation();
-    console.log(location)
-
-    return NextResponse.json(location)
-
-}
+  return NextResponse.json(location);
+};
