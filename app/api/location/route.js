703f93434f2dc8883479c8b76b2bb5b79dd722af
Shamiul40 <shamiul4040@gmail.com>
2026-02-18 23:18:05 +0600

    location api

---
+++ app/api/location/route.js
@@ -0,0 +1,12 @@
+import { NextResponse } from "next/server";
+import { getLocation } from "./location-utils"
+
+
+export const GET=async(request)=>{
+
+    const location = await getLocation();
+    console.log(location)
+
+    return NextResponse.json(location)
+
+}
