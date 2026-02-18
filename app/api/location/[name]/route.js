d65c65f62e9b49aeba1f1309b3bf30167e6759de
Shamiul40 <shamiul4040@gmail.com>
2026-02-18 23:24:08 +0600

    get data by name

---
+++ app/api/location/[name]/route.js
@@ -0,0 +1,11 @@
+import { NextResponse } from "next/server";
+import { getLocationByName } from "../location-utils";
+
+export const GET=async(request, {params})=>{
+
+    const name = params.name;
+    const locationByName =await getLocationByName(name);
+
+    return NextResponse.json(locationByName)
+
+}
