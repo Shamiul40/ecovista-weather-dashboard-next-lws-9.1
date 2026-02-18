4a6f4916c6f2641b0237add5a7f7fe07dcfc7e3a
Shamiul40 <shamiul4040@gmail.com>
2026-02-19 00:50:17 +0600

    change

--- app/api/location/[name]/route.js
+++ app/api/location/[name]/route.js
@@ -1,11 +1,9 @@
 import { NextResponse } from "next/server";
 import { getLocationByName } from "../location-utils";
 
-export const GET=async(request, {params})=>{
+export const GET = async (request, { params }) => {
+  const name = params.name;
+  const locationByName = await getLocationByName(name);
 
-    const name = params.name;
-    const locationByName =await getLocationByName(name);
-
-    return NextResponse.json(locationByName)
-
-}
+  return NextResponse.json(locationByName);
+};
