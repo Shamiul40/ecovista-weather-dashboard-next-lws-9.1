9cfc9fc95e00b68bfe2db4b289ad33090e6784ad
Shamiul40 <shamiul4040@gmail.com>
2026-02-20 17:54:26 +0600

    container all body and style

--- app/[location]/layout.js
+++ app/[location]/layout.js
@@ -1,17 +1,30 @@
 import Image from "next/image";
 import "../globals.css";
 
-export default function layout({children, aqi, weather, temperature,wind}) {
+export default function layout({ children, aqi, weather, temperature, wind }) {
   return (
-    <div className="grid min-h-screen place-content-center overflow-y-auto bg-[#19243B]" >
-      <Image className="absolute top-0 left-0 right-0 z-10 object-cover w-full h-[70vh] max-lg:fixed max-lg:min-h-screen  " src="/images/background.png" height={1200} width={700} alt="bg-image" />
+    <div className="grid min-h-screen place-content-center overflow-y-auto bg-[#19243B]">
+      <Image
+        className="absolute top-0 left-0 right-0 z-10 object-cover w-full h-[70vh] max-lg:fixed max-lg:min-h-screen  "
+        src="/images/background.png"
+        height={1200}
+        width={700}
+        alt="bg-image"
+      />
+
       <div className="absolute z-20 left-0 right-0 top-0 w-full h-[70vh] bg-[url('/images/gradient.png')] bg-cover bg-left-top bg-no-repeat max-lg:fixed max-lg:min-h-screen"></div>
 
-      {children}
-      {weather}
-      {aqi}
-      {wind}
-      {temperature}
+      <main className="!z-50 w-full">
+        <div className="container">
+          <div className="grid grid-cols-12 gap-y-16 py-16 lg:gap-8 2xl:gap-20 2xl:py-20">
+            {children}
+            {weather}
+            {aqi}
+            {wind}
+            {temperature}
+          </div>
+        </div>
+      </main>
     </div>
-  )
+  );
 }
