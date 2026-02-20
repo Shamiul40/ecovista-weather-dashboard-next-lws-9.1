a6dc015d7e883b7a7a37aa36029461a03fa06883
Shamiul40 <shamiul4040@gmail.com>
2026-02-21 00:10:03 +0600

    clean code

--- components/LocationDetector.js
+++ components/LocationDetector.js
@@ -1,87 +1,45 @@
-// "use client"
-
-// import Image from "next/image";
-// import { usePathname, useRouter, useSearchParams } from "next/navigation";
-// import React, { useEffect, useState } from "react";
-
-// export default function LocationDetector() {
-//   const [loading, setLoading] = useState(false);
-//   const searchParams = useSearchParams();
-//   const pathName = usePathname();
-//   const router = useRouter();
-
-//   useEffect(() => {
-//     setLoading(true);
-
-//     const params = new URLSearchParams(searchParams)
-
-//     if(navigator.geolocation) {
-//         navigator.geolocation.getCurrentPosition((position)=>{
-//             params.set('latitude', position.coords.latitude)
-//             params.set("longitude", position.coords.longitude)
-
-//             setLoading(false)
-
-//             router.push(`/current?${params.toString()}`)
-            
-
-//         })
-//     }
-
-//   }, [pathName, searchParams]);
-
-//   return (
-//   <div className="flex flex-col justify-center items-center h-screen bg-slate-700 text-white">
-
-//     {
-//         loading && (
-//             <>
-//             <Image alt="image" src='/network.gif' height={500} width={500} className="border rouded-md my-4" />
-//             </>
-//         )
-//     }
-//   </div>);
-// }
-
-
 "use client"
 
-import Image from 'next/image';
-import { usePathname, useRouter, useSearchParams } from 'next/navigation';
-import React, { useEffect, useState } from 'react'
+import Image from "next/image";
+import { usePathname, useRouter, useSearchParams } from "next/navigation";
+import React, { useEffect, useState } from "react";
 
 export default function LocationDetector() {
+  const [loading, setLoading] = useState(false);
+  const searchParams = useSearchParams();
+  const pathName = usePathname();
+  const router = useRouter();
 
-    const [loading, setLoading] = useState(false);
-    const searchParams = useSearchParams();
-    const pathName = usePathname()
-    const router=useRouter();
+  useEffect(() => {
+    setLoading(true);
 
-    useEffect(()=>{
-        setLoading(true)
+    const params = new URLSearchParams(searchParams)
 
-        const params = new URLSearchParams(searchParams)
-        
-        if(navigator.geolocation) {
-            navigator.geolocation.getCurrentPosition(position=>{
-                params.set("latitude", position.coords.latitude)
-                params.set("longitude", position.coords.longitude)
-                
-                setLoading(false);
+    if(navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition((position)=>{
+            params.set('latitude', position.coords.latitude)
+            params.set("longitude", position.coords.longitude)
 
-                router.push(`/current${params.toString()}`)
+            setLoading(false)
 
-            })
+            router.push(`/current?${params.toString()}`)
+            
 
-        
-
+        })
     }
 
-    },[searchParams, router])
-    
+  }, [pathName, searchParams]);
+
   return (
-    <div>
-      <Image alt="img" src="/network.gif" height={500} width={500} />
-    </div>
-  )
+  <div className="flex flex-col justify-center items-center h-screen bg-slate-700 text-white">
+
+    {
+        loading && (
+            <>
+            <Image alt="image" src='/network.gif' height={500} width={500} className="border rouded-md my-4" />
+            </>
+        )
+    }
+  </div>);
 }
+
