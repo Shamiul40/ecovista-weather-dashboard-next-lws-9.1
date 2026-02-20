a44840cbd9ecfe8593e7a5503038e5737943262f
Shamiul40 <shamiul4040@gmail.com>
2026-02-21 00:05:05 +0600

    repeate code practice

--- components/LocationDetector.js
+++ components/LocationDetector.js
@@ -46,27 +46,42 @@
 
 "use client"
 
-import { usePathname, useSearchParams } from 'next/navigation';
-import React, { useState } from 'react'
+import Image from 'next/image';
+import { usePathname, useRouter, useSearchParams } from 'next/navigation';
+import React, { useEffect, useState } from 'react'
 
 export default function LocationDetector() {
 
     const [loading, setLoading] = useState(false);
     const searchParams = useSearchParams();
     const pathName = usePathname()
+    const router=useRouter();
 
-    if(navigator.geolocation) {
+    useEffect(()=>{
         setLoading(true)
+
         const params = new URLSearchParams(searchParams)
+        
+        if(navigator.geolocation) {
+            navigator.geolocation.getCurrentPosition(position=>{
+                params.set("latitude", position.coords.latitude)
+                params.set("longitude", position.coords.longitude)
+                
+                setLoading(false);
+
+                router.push(`/current${params.toString()}`)
+
+            })
 
         
 
     }
 
+    },[searchParams, router])
     
   return (
     <div>
-      
+      <Image alt="img" src="/network.gif" height={500} width={500} />
     </div>
   )
 }
