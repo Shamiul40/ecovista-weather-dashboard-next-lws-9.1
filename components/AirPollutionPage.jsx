9a5e841c3ae85f694111fcb514ad31cc255b8095
Shamiul40 <shamiul4040@gmail.com>
2026-02-23 21:09:44 +0600

    change

--- components/AirPollutionPage.jsx
+++ components/AirPollutionPage.jsx
@@ -74,8 +74,8 @@ export default async function AirPollutionPage({ lat, lon }) {
             {components.co}µg/m³
           </span>
         </div>
-        {/* <!-- item ends --> */}
-        {/* <!-- item --> */}
+       
+        
         <div className="flex items-center justify-between gap-4">
           <div className="flex items-center gap-2 text-sm text-[#CADEE8] lg:text-base">
             <Image
